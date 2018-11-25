822f4fb6c8a940dabcb19621863692087e375ec6
gaelkyeo <contact@kyeo.fr>
2018-11-25 20:27:54 +0100

    upload all files first commit

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"OOos%vuzx$dULA<=/_qUEDCmkP%wbMwdb.J7R>N:i|$f[VG!PT#HFS8q`[r(=FAr";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2613;}
